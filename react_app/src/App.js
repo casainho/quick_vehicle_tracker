ae700e9d59c5ba9117215684942da343f70b6f50
casainho <casainho@gmail.com>
2023-11-10 12:18:30 +0000

    React Header testing

---
+++ react_app/src/App.js
@@ -0,0 +1,15 @@
+import Header from './components/Header.js';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Header />
+      <Header />
+      <Header />
+    </div>
+  );
+}
+
+export default App;
